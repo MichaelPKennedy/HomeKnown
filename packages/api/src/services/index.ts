db235cc756df9a222b62fc74604b03b2e0880be0
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2023-08-22 22:22:31 -0400

    change search component

--- packages/api/src/services/index.ts
+++ packages/api/src/services/index.ts
@@ -1,8 +1,8 @@
-// import { places } from './places/places'
+import { places } from './places/places'
 // For more information about this file see https://dove.feathersjs.com/guides/cli/application.html#configure-functions
 import type { Application } from '../declarations'
 
 export const services = (app: Application) => {
-  // app.configure(places)
+  app.configure(places)
   // All services will be registered here
 }
--- packages/web/src/pages/SecondPage/index.js
+++ packages/web/src/pages/SecondPage/index.js
@@ -1,33 +1,43 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
 import client from "../../feathersClient.js";
 
 function PlacesComponent() {
   const [places, setPlaces] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await client.service("places").find();
-        setPlaces(response.data);
-        setLoading(false);
-      } catch (err) {
-        setError(err.message);
-        setLoading(false);
-      }
+  const fetchPlaces = async () => {
+    try {
+      setLoading(true);
+      const response = await client.service("places").find({
+        query: {
+          // Your query criteria here, for instance:
+          name: searchTerm,
+        },
+      });
+      setPlaces(response.data);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
     }
-
-    fetchData();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+  };
 
   return (
     <div>
       <h1>Places</h1>
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for a place..."
+        />
+        <button onClick={fetchPlaces}>Search</button>
+      </div>
+      {loading && <div>Loading...</div>}
+      {error && <div>Error: {error}</div>}
       <ul>
         {places.map((place) => (
           <li key={place.id}>{place.text}</li>
