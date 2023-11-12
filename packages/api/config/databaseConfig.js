1ce67a62df8e86615e89b4c2d57ca08bfc666e3f
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2023-11-12 14:14:14 -0500

    axios dependency

--- packages/api/config/databaseConfig.js
+++ packages/api/config/databaseConfig.js
@@ -8,14 +8,6 @@ const databaseConfig = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql'
-  },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql'
   }
 }
 
