dff73094a08b3bda485f4b599cb7318aad45d004
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2023-11-12 15:18:38 -0500

    debug logs

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -5,10 +5,14 @@ const logger_1 = require("./logger");
 // Heroku dynamically assigns a port via process.env.PORT
 const port = process.env.PORT || app_1.app.get('port');
 const host = app_1.app.get('host');
+console.log('port2', port);
+console.log(process.env);
+console.log('test');
 process.on('unhandledRejection', (reason) => {
     logger_1.logger.error('Unhandled Rejection %O', reason);
 });
 console.log('port', port);
+console.log(process.env);
 app_1.app.listen(port, () => {
     logger_1.logger.info(`Feathers app listening on http://${host}:${port}`);
 });
