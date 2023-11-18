9ce745e01fb5f6dc9520dbc1880f96d5b0d7528e
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2023-11-18 10:44:18 -0500

    add imports for demographics table

--- packages/web/src/pages/City/components/DemographicsTable.jsx
+++ packages/web/src/pages/City/components/DemographicsTable.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Card } from "react-bootstrap";
+import styles from "../City.module.css";
 
 const DemographicsTable = ({ data }) => {
   console.log("in table", data);
