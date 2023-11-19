f8d37f15280ac7aa790d0c1eaf21eb8365a7531a
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2023-11-19 10:30:14 -0500

    add loading screen image

--- packages/web/src/pages/HomePage/components/LoadingScreen.jsx
+++ packages/web/src/pages/HomePage/components/LoadingScreen.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./LoadingScreen.module.css";
+import logoImage from "../assets/CircleLogoHomeKnown.png";
 
 const LoadingScreen = () => {
   return (
     <div className={styles.loadingScreen}>
+      <img src={logoImage} alt="Loading" className={styles.logo} />
       <div className={styles.loadingText}>
         Please wait while we find you the perfect place to live
       </div>
