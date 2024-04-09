8352674b7c675dff645eaedc561a719f91c979ab
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2024-04-08 22:21:30 -0400

    heart styles improved

--- packages/web/src/components/HeartIcon.jsx
+++ packages/web/src/components/HeartIcon.jsx
@@ -1,7 +1,9 @@
 import styles from "./HeartIcon.module.css";
 
 function HeartIcon({ onClick, className, isSaved }) {
-  const fillColor = isSaved ? "black" : "none";
+  const fillColor = isSaved ? "#bf0000" : "#3b3b3b";
+  const strokeColor = isSaved ? "white" : "white";
+
   return (
     <button
       onClick={onClick}
@@ -20,6 +22,8 @@ function HeartIcon({ onClick, className, isSaved }) {
       >
         <path
           fill={fillColor}
+          stroke={strokeColor}
+          strokeWidth="15"
           d="M135.13,231.13h0C37.63,167.47-18.04,80.33,23.31,31.13c12.44-14.8,33.03-19.9,51.76-19.9s37.46,6.63,51.76,19.9l8.3,7.72,8.31-7.72c28.58-26.54,80.79-31.05,103.51,0,38.1,52.08,2.02,119.1-111.82,200Z"
         />
       </svg>
