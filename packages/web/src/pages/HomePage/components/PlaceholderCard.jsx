eed7a79460cc8df1a0f4d22461da1b2587310b4f
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2024-06-04 19:04:34 -0400

    align placeholder cards

--- packages/web/src/pages/HomePage/components/PlaceholderCard.jsx
+++ packages/web/src/pages/HomePage/components/PlaceholderCard.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./PlaceholderCard.module.css";
 
 const PlaceholderCard = ({ index }) => {
-  return <div className={styles.placeholderContainer} key={index}></div>;
+  return (
+    <div className={styles.placeholderContainer} key={index}>
+      <div className={styles.imageContainer}></div>
+    </div>
+  );
 };
 
 export default PlaceholderCard;
