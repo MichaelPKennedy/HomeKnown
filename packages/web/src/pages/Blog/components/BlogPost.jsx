496ed3cc740b4c7e0df7c13ab131bda72a700e66
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2024-07-06 14:40:17 -0400

    find blog post based on title

--- packages/web/src/pages/Blog/components/BlogPost.jsx
+++ packages/web/src/pages/Blog/components/BlogPost.jsx
@@ -1,22 +1,35 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { useBlog } from "../../../utils/BlogContext";
 import styles from "./BlogPost.module.css";
 
 const BlogPost = () => {
   const location = useLocation();
+  const { posts, loading, error } = useBlog();
+  const { title } = useParams();
   const { post } = location.state || {};
 
-  if (!post) {
+  const blogPost = post
+    ? post
+    : posts.find(
+        (post) => post.title.replace(/\s+/g, "-").toLowerCase() === title
+      );
+
+  if (!post && loading) return <div>Loading...</div>;
+  if (!post && error) return <div>Error loading post</div>;
+
+  if (!blogPost) {
     return <div>Post not found</div>;
   }
 
   return (
     <div className={styles.blogPostContainer}>
-      <h1>{post.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      <h1>{blogPost.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: blogPost.content }} />
       <p>
         <em>
-          By {post.author} on {new Date(post.createdAt).toLocaleDateString()}
+          By {blogPost.author} on{" "}
+          {new Date(blogPost.createdAt).toLocaleDateString()}
         </em>
       </p>
     </div>
