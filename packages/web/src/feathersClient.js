53090040ffcaec1b47b90b826d5b693ceadbe6b1
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2025-07-06 14:13:26 -0400

    Fix API endpoint to use production URL as default fallback

--- packages/web/src/feathersClient.js
+++ packages/web/src/feathersClient.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const isMobileTesting = process.env.VITE_MOBILE_TESTING;
 const DEFAULT_API_ENDPOINT = isMobileTesting
   ? "http://10.0.0.2:3030"
-  : "http://localhost:3030";
+  : "https://homeknown-api-4fe050b813af.herokuapp.com";
 
 const API_ENDPOINT = process.env.VITE_API_URL || DEFAULT_API_ENDPOINT;
 
